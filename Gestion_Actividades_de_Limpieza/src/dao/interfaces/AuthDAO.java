b0641b229f7a39157f083f996580c53e2f046b6b
RodrigoRamirez-N <rodry2810g@gmail.com>
2025-04-08 16:07:20 -0600

    Corrección procedure autenticar_user

--- Gestion_Actividades_de_Limpieza/src/dao/interfaces/AuthDAO.java
+++ Gestion_Actividades_de_Limpieza/src/dao/interfaces/AuthDAO.java
@@ -2,13 +2,9 @@ package dao.interfaces;
 
 import java.sql.SQLException;
 
-import model.Usuario;
-
 public interface AuthDAO {
-
     
-    Usuario autenticar(String nombre, String contrasena) throws SQLException; // Autenticar al usuario - verificar si existe en la base de datos y si la contraseña es correcta
+    boolean autenticar(String nombre, String contrasena) throws SQLException; // Autenticar al usuario - verificar si existe en la base de datos y si la contraseña es correcta
     void cerrarSesion(int idUsuario) throws SQLException; // Cerrar sesión del usuario - volver a la pantalla de inicio de sesión
-    Usuario obtenerUsuarioPorId(int idUsuario) throws SQLException; // Obtener información del usuario por su ID - para mostrar en la pantalla del menú como loggeado : opcional mas bien seria en la interfaz de usuario
 
 }
